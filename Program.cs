010a5b981c608cc7e2167835fcad2457f1871780
Olesya <velikod@inbox.ru>
2023-01-29 18:44:48 +0300

    написана программа

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача. Написать программу, которая из имеющегося массива строк формирует массив из 
+строк, длина которых меньше, либо равна 3 символам.*/
+
+    Console.Write("Введите количество строк, которые хотите ввести: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите строку №{0}:   ", i + 1);
+        array[i] = Console.ReadLine();
+    }
+    
+
+void ArrayNew(string[] arrayNew)
+{  
+    Console.WriteLine("Строки, длина которых меньше, либо равна 3:   "); 
+    for (int i = 0; i < n;  i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            arrayNew[i] = array[i]; 
+            Console.WriteLine(arrayNew[i]); 
+        }      
+    }
+}
+ArrayNew(array);
